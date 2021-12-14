b41aef87756f3c8515a81189a6ea5d61f81a0118
Federico Castañares <federico.castanares@gmail.com>
2021-12-14 17:45:35 -0300

    con control

--- controllers/orders/add.js
+++ controllers/orders/add.js
@@ -1,8 +1,41 @@
 const orderModel = require('../../models/order')
+const Joi = require('@hapi/joi')
 
-module.exports = async (request, response) => { 
+module.exports = async (request, response) => {
 
-    try {
+    const schema = Joi.object({
+        personalData: Joi.object({
+            aclaraciones: String,
+            day: String,
+            location: String,
+            name: String,
+            paymentMethod: String,
+            phone: String,
+            zone: String,
+        }),
+        cart: Joi.array()
+        /*.items(
+            Joi.object({
+                // Box
+                activeItems: Number,
+                aggregates: Array,
+                items: [{
+                    active: Boolean,
+                    kg: String,
+                    name: String,
+                    qty: String
+                }],
+                // Item
+                name: String,
+                price: String,
+                units: String
+            })
+        )*/
+    })
+    const validationResult = schema.validate(request.body);
+
+    if (!validationResult.error) {
+
         orderModel.create({
             personalData: request.body.personalData,
             cart: request.body.cart
@@ -18,7 +51,7 @@ module.exports = async (request, response) => {
                 })
             }
         })
-    } catch {
+    } else {
         console.log(validationResult.error);
         response.status(400).json({
             message: validationResult.error,

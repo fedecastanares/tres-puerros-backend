5591a926ef58942cbac4b4eb237d392de1210ab8
Federico Castañares <federico.castanares@gmail.com>
2022-02-10 14:33:38 -0300

    limit

--- controllers/orders/get.js
+++ controllers/orders/get.js
@@ -3,7 +3,7 @@ const orderModel = require('../../models/order')
 module.exports = async (request, response) => {
     try { 
         
-        const orders = await orderModel.find({delivered: false}, '-__v');
+        const orders = await orderModel.find({delivered: false}, '-__v').limit(100);
 
         response.json({
             orders
